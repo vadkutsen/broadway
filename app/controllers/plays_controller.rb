b50775e23e6ba94e1182933f9c1b3ed1332e77c5
vadkutsen <vadkutsen@gmail.com>
2018-07-17 17:05:30 +0200

    Add new and create actions

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,2 +1,25 @@
 class PlaysController < ApplicationController
+
+
+	def index
+	end
+
+	def new
+		@play = Play.new
+	end
+
+	def create
+		@play = Play.new(play_params)
+	end
+
+	def edit
+	end
+
+	
+
+	private
+
+	def play_params
+		params.require(:play).permit(:title, :description, :director)
+	end
 end
