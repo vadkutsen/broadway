a33fc460e4e4ed0e651bee1070946264ee257e8a
vadkutsen <vadkutsen@gmail.com>
2018-07-17 19:26:10 +0200

    Add show page

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,7 +1,11 @@
 class PlaysController < ApplicationController
-
+	before_action :find_play, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@plays = Play.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new
@@ -10,6 +14,12 @@ class PlaysController < ApplicationController
 
 	def create
 		@play = Play.new(play_params)
+		
+		if @play.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -21,5 +31,9 @@ class PlaysController < ApplicationController
 
 	def play_params
 		params.require(:play).permit(:title, :description, :director)
+	end
+
+	def find_play
+		@play = Play.find(params[:id])
 	end
 end
